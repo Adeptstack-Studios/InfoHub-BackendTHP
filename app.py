a026614e4bcef8cdd6b5d8376bc7072ab5f376e3
ProgrammerLP <developer.programmerlp@gmx.net>
2024-01-20 22:16:30 +0100

    added code for getting sensor values

--- app.py
+++ app.py
@@ -1,31 +1,61 @@
 from flask import Flask, jsonify, request
 from json import dumps
+from pigpio_dht import DHT11, DHT22
+import board
+import busio
+import adafruit_bmp280
+import digitalio #Using SPI
+import requests
 
 app = Flask(__name__)
 
+def getDHT22Values():
+    gpio = 4 # BCM Numbering
+    #sensor = DHT11(gpio)
+    sensor = DHT22(gpio)
+    result = sensor.sample(samples=1)
+    temp = result["temp_c"]
+    humidity = result["humidity"]
+    print(f"temperature: {temp}°C")
+    print(f"humidity: {humidity}%")
+    return temp, humidity
 
-@app.route('/')
-@app.route('/index')
-def welcome():
-    return "<p>Hello API World!</p>"
+def getBMP280Values():
+    # API Call for current Sealevel Pressure values
+    psea = 0
+    api_url = "" # Your url for an api call to retrieve the sea level pressure
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        api_data = response.json()
+        psea = api_data['current']['pressure_msl']
+    else:
+        print(f"Error during API call. Status code: {response.status_code}")
 
+    # Read sensor data
+    spi = board.SPI()
+    bmp_cs = digitalio.DigitalInOut(board.D5)
+    sensor = adafruit_bmp280.Adafruit_BMP280_SPI(spi, bmp_cs)
+    # This value must be changed to the current air pressure at your location
+    # otherwise there will be inaccuracies
+    # weather services can give you information
+    # 1013.25 hPa is the mean air pressure at sea level
+    sensor.sea_level_pressure = psea
+    # Output of the measured values
+    print("\nTemperatur: %0.1f *C" % sensor.temperature)
+    print("Luftdruck: %0.1f hPa" % sensor.pressure)
+    print("Hoehe: %0.2f m" % sensor.altitude)
+    pressure = round(sensor.temperature, 1)
+    altitude = round(sensor.altitude, 2)
+    return pressure, altitude
 
-@app.route('/get')
+@app.route('/GetCurrentValues')
 def send_json():
-    file = open("ok.json")
-    data = file.read()
-    file.close()
+    temperature, humidity = getDHT22Values()
+    pressure, altitude = getBMP280Values()
+    json = {"temperature": temperature, "humidity": humidity, "pressure": pressure, "altitude": altitude }
+
+    data = json.dumps(json)
     return data
-
-
-@app.route('/post', methods=['POST'])
-def receive_json():
-    data = request.get_json()
-    text = dumps(data)
-    file = open("ok.json", "w")
-    file.write(text)
-    file.close()
-    return text
 
 
 if __name__ == '__main__':
