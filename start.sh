d2addd9f16b0e4ead0aee466db10079f78f63876
ProgrammerLP <developer.programmerlp@gmx.net>
2024-01-20 22:49:54 +0100

    .

--- start.sh
+++ start.sh
@@ -6,6 +6,7 @@ sudo pip3 install pigpio-dht &&
 sudo pip3 install gunicorn && 
 sudo pip3 install adafruit-circuitpython-bmp280 && 
 sudo pip3 install adafruit-circuitpython-lis3dh
+sudo pigpiod
 
 echo
 echo don\'t forget to activate spi interface!
