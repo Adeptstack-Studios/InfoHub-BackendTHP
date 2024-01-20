f051d34b800784396a77a1a8783ff4778261fc4d
ProgrammerLP <developer.programmerlp@gmx.net>
2024-01-20 22:31:22 +0100

    added installation script

---
+++ start.sh
@@ -0,0 +1,11 @@
+sudo apt-get update && 
+sudo apt-get install python3-pip && 
+sudo apt-get install git && 
+sudo pip3 install flask && 
+sudo pip3 install pigpio-dht && 
+sudo pip3 install gunicorn && 
+sudo pip3 install adafruit-circuitpython-bmp280 && 
+sudo pip3 install adafruit-circuitpython-lis3dh
+
+echo don't forget to activate spi interface!
+echo cd in git repository
