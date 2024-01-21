113ca2dbe4be9e5f2a2ef6ffd1a43a41ba138121
ProgrammerLP <developer.programmerlp@gmx.net>
2024-01-21 22:34:45 +0100

    update

--- installation.sh
+++ installation.sh
@@ -4,10 +4,12 @@ sudo apt-get install git &&
 sudo apt install screen && 
 sudo pip3 install flask && 
 sudo pip3 install pigpio-dht && 
+sudo apt-get install -y pigpio &&
 sudo pip3 install gunicorn && 
 sudo pip3 install adafruit-circuitpython-bmp280 && 
 sudo pip3 install adafruit-circuitpython-lis3dh
 
 echo
 echo don\'t forget to activate spi interface!
+echo please set an api url in app.py
 echo
