2b47ed27882dbe4aaed5097e162825e055e2c706
ProgrammerLP <developer.programmerlp@gmx.net>
2024-01-21 17:16:33 +0100

    fixes

--- installation.sh
+++ installation.sh
@@ -6,11 +6,6 @@ sudo pip3 install pigpio-dht &&
 sudo pip3 install gunicorn && 
 sudo pip3 install adafruit-circuitpython-bmp280 && 
 sudo pip3 install adafruit-circuitpython-lis3dh
-sudo pigpiod
-
-sudo mv InfoHubAutoStart /etc/init.d
-sudo chmod 755 /etc/init.d/InfoHubAutoStart
-sudo update-rc.d InfoHubAutoStart defaults
 
 echo
 echo don\'t forget to activate spi interface!
